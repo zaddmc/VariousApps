0954f748adaa59fc564ea223b450dc846391d615
Zadd ZC <davidbk927@gmail.com>
2023-09-06 20:17:48 +0200

    new and improved (completed) file reading system ready to be utalized

--- CrosswordFixer/Construction.cs
+++ CrosswordFixer/Construction.cs
@@ -1,6 +1,7 @@
-﻿namespace CrosswordFixer {
+﻿using System.Diagnostics;
+namespace CrosswordFixer {
     internal class Construction {
-        private static string[][] Letters { get; set; }
+        private static string[][] Letters { get; set; } // The first bracket is Row/y while the second is Col/x
         private static string[] PotentielWords { get; set; }
 
 
@@ -20,27 +21,65 @@
 
             StreamReader sr = new StreamReader(file);
 
+            while (true) {
+                Debug.WriteLine("Checking File for next step");
+                try {
+                    switch (sr.ReadLine().ToLower()) {
+                        case "read":
+                            Debug.WriteLine("Reading letters");
+                            ReadLetters(sr);
+                            break;
+                        case "words":
+                            Debug.WriteLine("Reading potentiel words");
+                            ReadWords(sr);
+                            break;
+                        case "finishedreading":
+                            Debug.WriteLine("finished reading the file will now continue");
+                            return;
+                        default:
+                            Debug.WriteLine("Empty Line or unrecognized determing wether to end or continue");
+                            if (sr.EndOfStream)
+                                return;
+                            break;
+                    }
+                }
+                catch (Exception) {
+                    Debug.WriteLine("Failed to read from file, may be due to it being empty or non existent");
 
-            int linesToRead = 0;
+                    throw;
+                }
+            }
+        }
+        private static void ReadWords(StreamReader sr) {
+            switch (sr.ReadLine().ToLower()) {
+                case "line":
+                    Debug.WriteLine("Reading potentiel words as a line");
+                    PotentielWords = sr.ReadLine().ToLower().Split(',');
+                    if (sr.ReadLine().ToLower() == "endwords")
+                        return;
+                    else
+                        Debug.WriteLine("Eat shit and die"); // can be made better to actually take care of the problem, but then again it is YOUR formatting that fucked up
+                    break;
+                case "list":
+                    Debug.WriteLine("to be done");
+                    break;
+            }
+        }
+        private static void ReadLetters(StreamReader sr) {
+            List<string[]> listOfLetters = new();
 
-            if (!int.TryParse(sr.ReadLine().Split(':')[1], out linesToRead))
-                throw new ArgumentException("Paramatar Fucked");
-
-            
-            Letters = new string[linesToRead][];
-
-            for (int i = 0; i < linesToRead; i++) {
+            int i = 0;
+            while (true) {
                 string toBeAllocated = sr.ReadLine();
 
                 if (toBeAllocated == "ENDREAD")
                     break;
-                
-                Letters[i] = toBeAllocated.ToLower().Split(',');
+
+                listOfLetters.Add(toBeAllocated.ToLower().Split(','));
+                i++;
             }
 
-            
-
-
+            Letters = listOfLetters.ToArray();
         }
 
     }
