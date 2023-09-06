3641a249c09194b58816ee4ec4e5a8997e9fcca7
Zadd ZC <davidbk927@gmail.com>
2023-09-06 19:07:55 +0200

    Reading letters has been implemented but new method of doing so is wanted so this will probalby be changed

--- CrosswordFixer/Construction.cs
+++ CrosswordFixer/Construction.cs
@@ -1,18 +1,47 @@
 ﻿namespace CrosswordFixer {
     internal class Construction {
+        private static string[][] Letters { get; set; }
+        private static string[] PotentielWords { get; set; }
+
+
         public static void GridMaker() {
 
-            MainPage.CrossGrid.SetRow(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) }, 0);
-            Grid crossGg = new Grid
-            {
-                RowDefinitions =
-                {
-                    new RowDefinition {Height = new GridLength(2, GridUnitType.Star)}
-                }
-            };
+            // not final just testing
+            FileReader(@"C:\Users\david\Source\Repos\zaddmc\CrosswordFixer\CrosswordFixer\Resources\Crosswords\test1.csv");
+
+
+
+            MainPage.CrossGrid.AddRowDefinition(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            MainPage.CrossGrid.AddColumnDefinition(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
         }
+        private static void FileReader(string file) {
 
+            StreamReader sr = new StreamReader(file);
+
+
+            int linesToRead = 0;
+
+            if (!int.TryParse(sr.ReadLine().Split(':')[1], out linesToRead))
+                throw new ArgumentException("Paramatar Fucked");
+
+            
+            Letters = new string[linesToRead][];
+
+            for (int i = 0; i < linesToRead; i++) {
+                string toBeAllocated = sr.ReadLine();
+
+                if (toBeAllocated == "ENDREAD")
+                    break;
+                
+                Letters[i] = toBeAllocated.ToLower().Split(',');
+            }
+
+            
+
+
+        }
 
     }
 }
--- CrosswordFixer/MainPage.xaml.cs
+++ CrosswordFixer/MainPage.xaml.cs
@@ -2,12 +2,13 @@
 
 public partial class MainPage : ContentPage {
 	public static Grid CrossGrid { get; private set; }
+	public static Grid MainGrid { get; private set; }
 
 	public MainPage() {
 		InitializeComponent();
 
 		CrossGrid = crossGrid;
-
+		MainGrid = maingrid;
 
 	}
 
