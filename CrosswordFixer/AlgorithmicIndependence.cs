7734e69dfa5ed68e03038a042f8d4fb246ea8555
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-09-08 08:37:17 +0200

    Have begun simple ai-algorithm

--- CrosswordFixer/AlgorithmicIndependence.cs
+++ CrosswordFixer/AlgorithmicIndependence.cs
@@ -1,22 +1,49 @@
-﻿namespace CrosswordFixer {
-    internal class AlgorithmicIndependence {
+﻿namespace CrosswordFixer
+{
+    internal class AlgorithmicIndependence
+    {
         // asbjorn is weak
         private static int number = 0;
 
 
-        public static void JustForTesting() {
-            for (int i = 0; i < Construction.Letters.Length-5; i++) {
+        public static void JustForTesting()
+        {
+            for (int i = 0; i < Construction.Letters.Length - 5; i++)
+            {
                 Worker.UnSelectAll();
                 Worker.PickRoot(number, 0);
-                for (int j = 0; j < 2; j++) {
+                for (int j = 0; j < 2; j++)
+                {
                     Worker.AddBranch(i, j);
                 }
 
             }
-        
-        
-        number++;
-        }
 
+
+            number++;
+        }
+        public static void SimpleAI()
+        {
+            List<string> cwords = Construction.PotentielWords.ToList();
+            while (true)
+            {
+                if (Worker.PickRoot(2, 4) == cwords.)
+                {
+
+                }
+
+            }
+ 
+
+
+
+
+            //Den checker første bogstav. hvis det ikke er det samme går den videre til den næste. Hvis det er det rigtige bogstav,
+            //så checker den de 3 felter over én, så til venstre, højre og 3 under. hvis der ikke er noget så går den videre til den næste.
+            //Hvis den er rigtig går til den næste
+
+
+
+        }
     }
 }
