1592f43527a55741e880c5b800c7d88df730dc9d
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-09-08 09:03:33 +0200

    nesting for and while lops. but no while loops

--- CrosswordFixer/AlgorithmicIndependence.cs
+++ CrosswordFixer/AlgorithmicIndependence.cs
@@ -1,22 +1,16 @@
-
-﻿namespace CrosswordFixer {
-    public class AlgorithmicIndependence {
-
-﻿namespace CrosswordFixer {
+//using Construction;
+namespace CrosswordFixer {
     public class AlgorithmicIndependence {
 
         // asbjorn is weak
         private static int number = 0;
 
 
-        public static void JustForTesting()
-        {
-            for (int i = 0; i < Construction.Letters.Length - 5; i++)
-            {
+        public static void JustForTesting() {
+            for (int i = 0; i < Construction.Letters.Length - 5; i++) {
                 Worker.UnSelectAll();
                 Worker.PickRoot(number, 0);
-                for (int j = 0; j < 2; j++)
-                {
+                for (int j = 0; j < 2; j++) {
                     Worker.AddBranch(i, j);
                 }
 
@@ -25,28 +19,26 @@
 
             number++;
         }
-        public static void SimpleAI()
-        {
+        public static void SimpleAI() {
             List<string> cwords = Construction.PotentielWords.ToList();
-            while (true)
-            {
-                if (Worker.PickRoot(2, 4) == cwords.)
-                {
+            for (int i = 0; i < Construction.Letters.Length; i++) {
+                for (int j = 0; j < Construction.Letters[i].Length; j++) {
+                    if (Worker.PickRoot(i, j) == cwords[0][0].ToString()) {//cwords does not work proberly by channging
+                        Position korrekt = new Position(i, j);
+                        
 
+                    }
+                    else
                 }
 
+
+
+                //Den checker første bogstav. hvis det ikke er det samme går den videre til den næste. Hvis det er det rigtige bogstav,
+                //så checker den de 3 felter over én, så til venstre, højre og 3 under. hvis der ikke er noget så går den videre til den næste.
+                //Hvis den er rigtig går til den næste
+
+
+
             }
- 
-
-
-
-
-            //Den checker første bogstav. hvis det ikke er det samme går den videre til den næste. Hvis det er det rigtige bogstav,
-            //så checker den de 3 felter over én, så til venstre, højre og 3 under. hvis der ikke er noget så går den videre til den næste.
-            //Hvis den er rigtig går til den næste
-
-
-
         }
     }
-}
