a8852496d48771f8e7bfd3e73f45900408e1a5d5
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-09-12 09:06:14 +0200

    Simplai expansion. .tostring needs "()". i forhor. remember next time. becoming closer to find the next words when looking down. this text will not make sense in the futura.

--- CrosswordFixer/AlgorithmicIndependence.cs
+++ CrosswordFixer/AlgorithmicIndependence.cs
@@ -1,4 +1,5 @@
 //using Construction;
+
 namespace CrosswordFixer {
     public class AlgorithmicIndependence {
 
@@ -21,14 +22,20 @@ namespace CrosswordFixer {
         }
         public static void SimpleAI() {
             List<string> cwords = Construction.PotentielWords.ToList();
-            for (int i = 0; i < Construction.Letters.Length; i++) {
+            for (int i = 0; i < Construction.Letters.Length; i++) { //i ned af, j er henad
                 for (int j = 0; j < Construction.Letters[i].Length; j++) {
-                    if (Worker.PickRoot(i, j) == cwords[0][0].ToString()) {//cwords does not work proberly by channging
-                        Position korrekt = new Position(i, j);
-                        
+                    if (Worker.PickRoot(i, j) == cwords[0][0].ToString()) {//Checks if the first letter is equel with the first letter of Potentiel words       cwords does not work proberly by channging. 
+                                                                           //Position korrekt = new Position(i, j);
+                        while (true) {
+                            SimpleAICheckAround(i, j);
 
+
+                        }
                     }
-                    else
+                    else {
+                        return;
+                    }
+
                 }
 
 
@@ -41,4 +48,52 @@ namespace CrosswordFixer {
 
             }
         }
+        public static (bool buh, int idkman) SimpleAICheckAround(int i, int j, List<string> cwords, int listnumber, int charnumber) {
+            for (int k = -1; k < 2; k++) {
+                if (Worker.PickRoot(i-1, j+k) == cwords[listnumber][1].ToString()) { //checker det næste bågstav ved de 3 neder under root, AKA en række ned
+                    SimpleAICheckNext(i, j, k, cwords, listnumber, charnumber);
+
+                }
+
+
+            }
+
+            return (true, 12);
+        }
+        public static bool SimpleAICheckNext(int i, int j, int k, List<string> cwords, int listnumber, int charnumber) {
+            if (Worker.PickRoot(i + k, j) == cwords[listnumber][charnumber].ToString())
+                if (Worker.PickRoot(i, j + k) == cwords[listnumber][cwords[listnumber].Length].ToString())
+
+                   
+
+            //cwords[listnumber][gghg
+            //[cwords[listnumber].Length].ToString;
+            
+            
+            /*switch (idkman) {
+            case -1:
+                if (Worker.PickRoot(i - 1, j) == cwords[listnumber][charnumber].ToString()) { //checker oppe venstre
+
+                }
+                break;
+            case 0:
+                if (Worker.PickRoot(i - 0, j) == cwords[listnumber][charnumber].ToString()) { //checker oppe midden
+
+                }
+                break;
+            case 1:
+                if (Worker.PickRoot(i + 1, j) == cwords[listnumber][charnumber].ToString()) { //checker oppe højre
+
+                }
+                break;
+            default:
+
+                break;
+
+        }*/
+            if (i == 0) { }
+
+            return (true);
+        }
     }
+}
