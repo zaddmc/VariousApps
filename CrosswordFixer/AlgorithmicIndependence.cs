8c652826b62da1a98ca51893223080f11cd6f2a4
Zadd ZC <davidbk927@gmail.com>
2023-09-12 08:43:23 +0200

    Better initialzation buttons to start an Algorithim

--- CrosswordFixer/AlgorithmicIndependence.cs
+++ CrosswordFixer/AlgorithmicIndependence.cs
@@ -3,9 +3,27 @@
         // asbjorn is weak
         private static int number = 0;
 
+        public static void DoButton(string name) {
+            Button button = new Button
+            {
+                ClassId = name,
+                Text = "Start: " + name + " Algorithim",
+                TextColor = Colors.Black,
+            };
+
+            MainPage.StartButtons.Add(button);
+        }
+        public static void MakeButtons() {
+            DoButton("testing");
+            DoButton("fishing");
+        }
+
+
+
+
 
         public static void JustForTesting() {
-            for (int i = 0; i < Construction.Letters.Length-5; i++) {
+            for (int i = 0; i < Construction.Letters.Length - 5; i++) {
                 Worker.UnSelectAll();
                 Worker.PickRoot(number, 0);
                 for (int j = 0; j < 2; j++) {
@@ -13,9 +31,9 @@
                 }
 
             }
-        
-        
-        number++;
+
+
+            number++;
         }
 
     }
--- CrosswordFixer/MainPage.xaml.cs
+++ CrosswordFixer/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace CrosswordFixer;
+﻿using System.Diagnostics;
+
+namespace CrosswordFixer;
 
 public partial class MainPage : ContentPage {
     public static Grid CrossGrid { get; private set; }
     public static Grid MainGrid { get; private set; }
+    public static VerticalStackLayout InitiazeButtons { get; private set; }
     static public Color[] ColorTheme { get; private set; }
     static public Dictionary<string, Label> Tiles { get; private set; }
+    static public List<Button> StartButtons { get; set; }
 
     static public bool IsEditModeEnabled = false;
 
@@ -13,14 +17,34 @@ public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
 
+        // initialzation of grid or other layouts to be used in the code
         CrossGrid = crossGrid;
         MainGrid = maingrid;
+        InitiazeButtons = initiazeButtons;
 
+        // initialzation of certain all seeing types 
         ColorTheme = new Color[] { Color.FromArgb("ffffff"), Color.FromArgb("2b2d31"), Color.FromArgb("1e1f22"), Colors.Green }; // Brightmode 
         Tiles = new Dictionary<string, Label>();
+        StartButtons = new List<Button>();
 
+        // initialzation of the algorithim buttons so that the user can start an algorithim
+        AlgorithmicIndependence.MakeButtons();
+        SetupAIButtons();
     }
+    private static void SetupAIButtons() {
 
+        while (StartButtons.Count > 0) {
+            Button button = StartButtons.First();
+            StartButtons.Remove(button);
+            
+            
+            button.Clicked += InitiazeButtons_Clicked;
+
+
+
+            InitiazeButtons.Children.Add(button);
+        }
+    }
     private async void Button_Clicked(object sender, EventArgs e) {
         string action = await DisplayActionSheet("Choose method to insert values", null, null, "ReadFile", "ManuelInput");
 
@@ -41,8 +65,23 @@ public partial class MainPage : ContentPage {
         }
     }
 
-    private void startTest_Clicked(object sender, EventArgs e) {
-        AlgorithmicIndependence.JustForTesting();
+    private static void InitiazeButtons_Clicked(object sender, EventArgs e) {
+
+        var button = sender as Button;
+        string identifier = button.ClassId;
+
+        switch (identifier) {
+            default:
+                Debug.WriteLine("Missing Identifier in the initialzation of AI buttons");
+                break;
+            case "testing":
+                AlgorithmicIndependence.JustForTesting();
+                break;
+            case "fishing":
+                AlgorithmicIndependence.JustForTesting();
+                break;
+        }
+
     }
 }
 
