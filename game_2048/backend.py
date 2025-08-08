d68b32812ed140388096eb9beae3fb9805837fab
zaddmc <davidbk927@gmail.com>
2025-08-08 15:15:18 +0200

    the tiles spawn but doesnt move

--- game_2048/backend.py
+++ game_2048/backend.py
@@ -1,3 +1,5 @@
+import random
+
 from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -14,11 +16,50 @@ class Backend(GridLayout):
         self.grid_size = grid_size
 
         for _ in range(grid_size * grid_size):
-            self.add_widget(Tile("2048"))
+            self.add_widget(Tile(""))
+
+        self.add_random()
+        self.add_random()
 
     def add_random(self):
-        self.add_widget(Tile())
-        self.add_widget(Tile())
+
+        try:
+            index_to_change = random.choice(list(self.get_free_tiles()))
+        except IndexError:
+            raise Exception("Game Over, poor loser")
+
+        self.remove_widget(self.children[index_to_change])
+        self.add_widget(Tile("2"), index_to_change)
+
+    def get_free_tiles(self):
+        for child in self.children:
+            if child.text == "":
+                yield child.get_index()
+
+    def take_input(self, value: str):
+        match value:
+            case "right":
+                pass
+            case "left":
+                pass
+            case "up":
+                pass
+            case "down":
+                self.move_tiles("down")
+            case _:
+                raise ValueError(f"Unexpected: {value}")
+
+        self.add_random()
+
+    def move_tiles(self, direction):
+        print(self.get_items(direction))
+
+    def get_items(self, direction):
+        lst = self.children
+        return [
+            lst[i * self.grid_size : (i + 1) * self.grid_size]
+            for i in range(self.grid_size)
+        ]
 
 
 class Tile(Button):
@@ -26,7 +67,10 @@ class Tile(Button):
         super().__init__(**kwargs)
         self.background_color = rgba("#555555")
         self.text = value
-        self.bind(font_size=self.resize, size=self.resize)
+        self.bind(size=self.resize)
 
     def resize(self, *args):
         self.font_size = self.width / 3
+
+    def get_index(self):
+        return self.parent.children.index(self)
--- game_2048/main.py
+++ game_2048/main.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.core.window import Window
+from kivy.core.window import Keyboard, Window
 from kivy.uix.floatlayout import FloatLayout
 
 from backend import Backend
@@ -14,10 +14,22 @@ class G2048(FloatLayout):
         self.backend = Backend(4)
         self.add_widget(self.backend)
 
+    def on_key_down(self, window, keycode, text, modifiers, is_repeat, *args):
+        if keycode in [Keyboard.keycodes["right"], Keyboard.keycodes["d"]]:
+            self.backend.take_input("right")
+        if keycode in [Keyboard.keycodes["left"], Keyboard.keycodes["a"]]:
+            self.backend.take_input("left")
+        if keycode in [Keyboard.keycodes["up"], Keyboard.keycodes["w"]]:
+            self.backend.take_input("up")
+        if keycode in [Keyboard.keycodes["down"], Keyboard.keycodes["s"]]:
+            self.backend.take_input("down")
+
 
 class G2048App(App):
     def build(self):
-        return G2048()
+        root = G2048()
+        Window.bind(on_key_down=root.on_key_down)
+        return root
 
 
 if __name__ == "__main__":
